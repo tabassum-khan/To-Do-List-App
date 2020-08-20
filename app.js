e2b0a8f5c3cca03b84174965202d4526898b6398
Tabassum Khan <tabassum.kamal.k@gmail.com>
2020-08-20 14:57:32 +0530

    Fixed typos in app.js

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const app = express();
 // Static Files
 app.use(express.static("./public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.render("index");
 })
 
-const PORT = env.process.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 let server = app.listen(PORT, () => console.log("Server is running on port " + PORT));

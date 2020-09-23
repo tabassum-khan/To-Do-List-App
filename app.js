853ee6aa950581004ff239a1dc1c881bfe1abd62
Tabassum Khan <tabassum.kamal.k@gmail.com>
2020-09-23 21:29:42 +0530

    Ddeploy heroku app

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(express.static("public"));
 //fire controllers
 todoController(app);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("Server is running on port " + PORT));
